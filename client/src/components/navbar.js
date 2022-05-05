dcd51fa2c899c63b661083074ea2cd894994304c
Sam LaChapelle <sambosauce@gmail.com>
2022-05-05 17:30:35 -0400

    NA issue 7

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -92,16 +92,16 @@ const actions = [
 ];
 
 function NavBar() {
-  // window.addEventListener(
-  //   "scroll",
-  //   () => {
-  //     document.body.style.setProperty(
-  //       "--scroll",
-  //       window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
-  //     );
-  //   },
-  //   false
-  // );
+  window.addEventListener(
+    "scroll",
+    () => {
+      document.body.style.setProperty(
+        "--scroll",
+        window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+      );
+    },
+    false
+  );
 
   return (
     <div>
